ae5c635457d00919f7324d223598828e53759f7b
Bruno Lindoso <brunolindoso@gmail.com>
2021-11-07 19:47:41 -0300

    04-quando-nao-usar-heranca

---
+++ playlist.py
@@ -0,0 +1,12 @@
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
