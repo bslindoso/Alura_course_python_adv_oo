1110c6dc62be1e44b3c1ae197706982f8ae3d01f
Bruno Lindoso <brunolindoso@gmail.com>
2021-11-09 07:12:59 -0300

    05-duck-typing-001

--- playlist.py
+++ playlist.py
@@ -3,6 +3,8 @@ class Playlist:
         self.nome = nome
         self._programas = programas
 
+    def __getitem__(self, item):
+        return self._programas[item]
     @property
     def listagem(self):
         return self._programas
